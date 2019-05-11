b608a57ef0fca4c846661e9ca6d7ce266f946a35
smanwarulislam <engg.anwarbd@gmail.com>
2019-05-11 16:35:22 +0600

    Primitives Exercises added

---
+++ Section 10 - Introduction to JavaScript/115. Primitives Exercises/JSPrimitivesProblemSet.js
@@ -0,0 +1,25 @@
+// JavaScript Basics
+// Primitives
+
+// Quick Exercises!
+// Evaluate the following statements
+
+// 1
+100 % 3
+
+// 2
+("blah" + "blah")[6]
+
+// 3
+"hello".length % "hi\\".length
+
+// Possible solutions
+
+// 1
+100 % 3 // 1
+
+// 2
+("blah" + "blah")[6] // "a"
+
+// 3
+"hello".length % "hi\\".length // 2
