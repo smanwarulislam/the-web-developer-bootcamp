cf717d02d05c098be96ce22ed4784fdd7567a6d2
smanwarulislam <engg.anwarbd@gmail.com>
2019-05-12 21:35:18 +0600

    Variables added

---
+++ Section 10 - Introduction to JavaScript/116. Variables/variables.js
@@ -0,0 +1,37 @@
+// Variables
+
+// Variables are simply containers that store values
+// They follow this pattern:
+var yourVariableName = yourValue;
+
+// They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+// Recall the stored value by calling the variable name
+var name = "Rusty";
+name // "Rusty"
+name = "Tater"; // "Tater"
+name // "Tater"
+
+var friend = "Sally";
+"hello there " + friend; // "hello there Sally"
+friend = "no one"; // "no one"
+"hello there " + friend; // "hello there no one"
+
+var num = 43;
+num // 43
+num + 100 // 143
+num % 2 // 1
+num = "fifty"; // "fifty"
+
+// We can also update existing variables
+var name = "Robert";
+name = "Bob";
+name // "Bob"
+
+/* Variable names convention
+camelCase
+snake_case
+kebab-case */
