ccbc8f3a68908ced6780d3a375eb2180f12169cb
smanwarulislam <engg.anwarbd@gmail.com>
2019-05-14 17:30:50 +0600

    Useful Built-In Methods added

---
+++ Section 10 - Introduction to JavaScript/118. Useful Built-In Methods/PromptAlertLog.js
@@ -0,0 +1,19 @@
+// Useful Built-In Methods
+
+// alert
+// prompt
+// console.log
+
+// alert
+alert('Hello there!!!');
+alert(1234567890);
+alert(198 * 345);
+
+// console.log
+console.log('hello from the console!'); // hello from the console!
+console.log(216378216 / 3); // 72126072
+
+// prompt
+prompt('What is your name?'); // "S M Anwarul Islam"
+var userName = prompt('What is your name?'); // Rusty
+userName; // "Rusty"
